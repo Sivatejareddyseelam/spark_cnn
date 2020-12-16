a35f8ee9ac4bc0ed12948c412e871683ef4b4048
Sivatejareddyseelam <seelam.s@husky.neu.edu>
2020-12-16 00:58:25 -0500

    initial push

---
+++ naive_training_test.py
@@ -0,0 +1,29 @@
+# test the performance of learnt CNN on its corresponding training data
+import sys
+import numpy as np
+from spark.utils import *
+from spark.cnn import CNN
+from time import time
+
+def test(size):
+    start = time()
+    cnn = CNN(0)
+    X, Y = load_training_data(0, size)
+    P = cnn.predict(X)
+    P = np.argmax(P, 1)
+    print('Testing naive CNN for %d training images' % (size))
+    print('Predicted Classifications:')
+    print(P)
+    print('Correct Classifications:')
+    print(Y)
+
+    C = np.concatenate([P, Y]).reshape(2, -1).T
+    C = [x for x in C if x[0] == x[1]]
+    print('Correct:')
+    print('%d/%d' % (len(C), size))
+    end = time()
+    print('Total time consumption: %.3f' % (end - start))
+
+if __name__ == '__main__':
+    size = int(sys.argv[1]) if len(sys.argv) > 1 else 10000
+    test(size)
